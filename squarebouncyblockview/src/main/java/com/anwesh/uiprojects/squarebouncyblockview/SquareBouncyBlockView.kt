d99acc216eec107e15030b2570c761b090c0b8c0
Anwesh43 <anweshthecool0@gmail.com>
2019-12-21 10:40:38 +0530

    adding constants and libraries

--- squarebouncyblockview/src/main/java/com/anwesh/uiprojects/squarebouncyblockview/SquareBouncyBlockView.kt
+++ squarebouncyblockview/src/main/java/com/anwesh/uiprojects/squarebouncyblockview/SquareBouncyBlockView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.squarebouncyblockview
 /**
  * Created by anweshmishra on 21/12/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val squares : Int = 4
+val scGap : Float = 0.005f
+val delay : Long = 30
+val foreColor : Int = Color.parseColor("#311B92")
+val backColor : Int = Color.parseColor("#BDBDBD")
