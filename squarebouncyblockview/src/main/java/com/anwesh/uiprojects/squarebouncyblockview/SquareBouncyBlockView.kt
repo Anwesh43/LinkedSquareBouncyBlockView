17b61893459837b3d0393ffd65faab51786fa408
Anwesh43 <anweshthecool0@gmail.com>
2019-12-21 11:21:23 +0530

    adding SquareBouncuBlock class

--- squarebouncyblockview/src/main/java/com/anwesh/uiprojects/squarebouncyblockview/SquareBouncyBlockView.kt
+++ squarebouncyblockview/src/main/java/com/anwesh/uiprojects/squarebouncyblockview/SquareBouncyBlockView.kt
@@ -156,4 +156,28 @@ class SquareBouncyBlockView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SquareBouncyBlock(var i : Int) {
+
+        private val root : SBBNode = SBBNode(0)
+        private var curr : SBBNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
