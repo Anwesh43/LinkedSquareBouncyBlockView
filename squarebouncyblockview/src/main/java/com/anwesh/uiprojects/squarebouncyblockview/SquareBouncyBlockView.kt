d8606920ac10d086286eed3a024940049fbaf455
Anwesh43 <anweshthecool0@gmail.com>
2019-12-21 11:08:38 +0530

    adding SquareBouncyBlockView class

--- squarebouncyblockview/src/main/java/com/anwesh/uiprojects/squarebouncyblockview/SquareBouncyBlockView.kt
+++ squarebouncyblockview/src/main/java/com/anwesh/uiprojects/squarebouncyblockview/SquareBouncyBlockView.kt
@@ -50,3 +50,20 @@ fun Canvas.drawSBBNode(i : Int, scale : Float, paint : Paint) {
     drawSquareBouncyBlocks(w, scale, paint)
     restore()
 }
+
+class SquareBouncyBlockView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
