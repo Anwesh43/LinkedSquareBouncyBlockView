f4815a5c677bf808e4832f0ad5dd4b981ed784d5
Anwesh43 <anweshthecool0@gmail.com>
2019-12-21 11:28:48 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsquarebouncyblockview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquarebouncyblockview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsquarebouncyblockview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.squarebouncyblockview.SquareBouncyBlockView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareBouncyBlockView.create(this)
     }
 }
