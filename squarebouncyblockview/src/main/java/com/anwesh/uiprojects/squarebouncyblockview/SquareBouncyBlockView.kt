0c6e45e7983ef87d13edf58df1e3af8f808de2ca
Anwesh43 <anweshthecool0@gmail.com>
2019-12-21 10:54:25 +0530

    adding Canvas extension methods

--- squarebouncyblockview/src/main/java/com/anwesh/uiprojects/squarebouncyblockview/SquareBouncyBlockView.kt
+++ squarebouncyblockview/src/main/java/com/anwesh/uiprojects/squarebouncyblockview/SquareBouncyBlockView.kt
@@ -24,3 +24,29 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n)
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSquareBouncyBlock(i : Int, gap : Float, size : Float, scale : Float, paint : Paint) {
+    val sc : Float = scale.sinify().divideScale(i, squares)
+    save()
+    translate(i * (gap + 1) + gap * sc, 0f)
+    drawRect(RectF(-size / 2, -size / 2, size / 2, size / 2), paint)
+    restore()
+}
+
+fun Canvas.drawSquareBouncyBlocks(w : Float, scale : Float, paint : Paint) {
+    val gap : Float = scale / (2 * squares + 1)
+    for (j in 0..(squares)) {
+        drawSquareBouncyBlock(j, gap, gap, scale, paint)
+    }
+}
+
+fun Canvas.drawSBBNode(i : Int, scale : Float, paint : Paint) {
+    val h : Float = height.toFloat()
+    val w : Float = width.toFloat()
+    paint.color = foreColor
+    val gap : Float = h / (nodes + 1)
+    save()
+    translate(0f, gap * (i + 1))
+    drawSquareBouncyBlocks(w, scale, paint)
+    restore()
+}
